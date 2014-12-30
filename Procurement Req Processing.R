f3e44550e863cb516c01018abededd689ecc1e80
unknown <vbspencer@nola.gov>
2014-12-30 16:52:56 -0600

    removed business day calculation

--- Procurement Req Processing.R
+++ Procurement Req Processing.R
@@ -1,4 +1,4 @@
-### This script is to generate the graph of business days for the Purchasing Bureau to convert requisitions into purchase orders, 
+### This script "WILL BE" to generate the graph of business days for the Purchasing Bureau to convert requisitions into purchase orders, 
 ## with a target of 4 business days.
 
 ##load relevant packages
@@ -16,14 +16,4 @@ Reqs<-Reqs[-c(1,2,3),]
 Reqs$FinanceDate<-as.Date(Reqs$FinanceDate,"%m/%d/%Y")
 Reqs$POdate<-as.Date(Reqs$POdate,"%m/%d/%Y")
 
-## set up business day calculation
-networkdays <- function(start, end, holidays) 
-  { 
-    dates <- seq(as.Date(start), as.Date(end), by="day")   
-      if(missing(holidays))holidays <- 0 else holidays <- length(holidays) 
-    sum(as.numeric(format(dates, "%w") > 1)) - holidays 
-    } 
-
-holidays<-c("2014-01-01","2014-01-20","2014-03-04","2014-04-18","2014-05-26","2014-07-04","2014-09-01","2014-11-27","2014-11-28","2014-12-25","2014-12-26")
-
-mutate(Reqs,businessdays=networkdays(FinanceDate,POdate,holidays))
+## set up business day calculation (WORK IN PROGRESS)
