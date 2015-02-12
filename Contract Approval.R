89f0ac67bb7b6587c0c6b4cdf5684640afa94def
Vspencer88 <vbspencer@nola.gov>
2015-02-11 21:19:33 -0600

    Added partial script on contract approval

--- Contract Approval.R
+++ Contract Approval.R
@@ -1,7 +1,7 @@
 require(tidyr)
 require(dplyr)
 require(lubridate)
-contracts<-read.csv("Contract Approval Sequence POs.csv",na.strings-c("",NA),skip=3)
+contracts<-read.csv("Contract Approval Sequence POs.csv",na.strings=c("",NA),skip=3)
 contracts<-select(contracts,AltID=ALTERNATE_ID,PO=PO_NBR,POdate=PO_DATE,AttorneyReview=PO_REQ_APP_DATE,ApprovalDate=APPROVAL_DATE,ApproverType=PO_APPROVER_TYPE,Approver=PO_APPROVER)
 contracts<-filter(contracts,Approver!="CLCSETTLEMYER",Approver!="CMJAVERILL",Approver!="CRDRIVERS",Approver!="CMESTRICKLAND",Approver!="CJECHRISTOPHER")
 
