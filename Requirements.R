13045930e5eb2948cb06987276435d5bd0c7fc56
unknown <vbspencer@nola.gov>
2015-09-21 13:19:35 -0500

    Added required packages for each component of the ReqtoCheck scripts

---
+++ Requirements.R
@@ -0,0 +1,13 @@
+library(tidyr) # Contracts
+library(plyr)  # All
+library(dplyr) # All
+library(lubridate) # All
+library(xlsx) # Contracts
+library(gdata)# Contracts
+library(stringr)# Contracts and payments
+library(r2excel)# Contracts (not sure if needed)
+library(reshape2)# Contracts and procurement
+library(zoo)# All
+library(ggplot2)# All
+library(gdata) # Payments
+library(bizdays)# Payments and Procurement
