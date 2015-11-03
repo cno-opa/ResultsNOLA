7860770af4c751b15cd2f1290dc0339ac3d5d3ff
unknown <vbspencer@nola.gov>
2015-11-03 17:07:28 -0600

    Tidied up plot aesthetics, changed write.csv directories

--- Procurement.R
+++ Procurement.R
@@ -33,13 +33,13 @@ POs$Over4<-ifelse(POs$WorkingDays<=4,0,1)
 
 ## Plot the business days to process by quarter
 Days2PO<-aggregate(data=POs,WorkingDays~Qtr,FUN=mean)
-Purchasing<-ggplot(Days2PO,aes(x=factor(Qtr),y=WorkingDays))
-Purchasing<-Purchasing+geom_bar(stat="identity",fill="steelblue")
-Purchasing<-Purchasing+ggtitle("Average Business Days to Process Purchase Orders")
-Purchasing<-Purchasing+xlab("Quarters")
-Purchasing<-Purchasing+ylab("Business Days")
-Purchasing<-Purchasing+geom_text(aes(y=WorkingDays,ymax=WorkingDays+1,label=round(WorkingDays,2)),position=position_dodge(width=0.9),vjust=-.5,size=5)
-Purchasing<-Purchasing+geom_hline(aes(yintercept=4,colour="#FF0000"),linetype=2,size=1)
+Purchasing<-ggplot(Days2PO,aes(x=factor(Qtr),y=WorkingDays))+
+    geom_bar(stat="identity",fill="steelblue")+
+      ggtitle("Average Business Days to Process Purchase Orders")+
+          xlab("Quarters")+ylab("Business Days")+
+              geom_text(aes(y=WorkingDays,ymax=WorkingDays+1,label=round(WorkingDays,2)),position=position_dodge(width=0.9),vjust=-.5,size=5)+
+                geom_hline(aes(yintercept=4,colour="#FF0000"),linetype=2,size=1)+
+                  theme(plot.title=element_text(size=13,face="bold",vjust=1))   
 print(Purchasing)
 ggsave("./ReqtoCheckSTAT/Query Files/Slides/Procurement/Days to PO.png")
 
@@ -57,7 +57,7 @@ POdist$position<-ifelse(POdist$variable=="Under_4",undermaxPO-.10,1-((1-undermax
 Dist_plot<-ggplot(POdist,aes(x = factor(Qtr), y = value,fill = variable)) + 
   geom_bar(position = "stack",stat = "identity") + 
     scale_y_continuous(labels = percent_format())+
-       ggtitle("Distribution of Business Days to Process Purchase Orders")+
+       ggtitle("Distribution of Business Days to \nProcess Purchase Orders")+
           xlab("Quarters")+ylab("Percent")+
              geom_text(aes(ymax=value,y=position,label=percent(value)),size=4)+
                  scale_fill_manual(values=c(lightBlue,red),name=" ",labels=c("<=4 Business Days",">4 Business Days"))
@@ -67,18 +67,22 @@ ggsave("./ReqtoCheckSTAT/Query Files/Slides/Procurement/PO Distribution.png")
 
 ## Plot business days to process by Buyer
 r_period<-max(POs$Qtr)
-POs$Buyer2<-ifelse(POs$Buyer=="Bernice Ealy","BE",
-                    ifelse(POs$Buyer=="Kai Wells","KW",
-                           ifelse(POs$Buyer=="Burma Jackson","BJ",
-                                  ifelse(POs$Buyer=="Stephanie Warren","SW",NA))))
+POs$Buyer2<-ifelse(POs$Buyer=="Bernice Ealy","Ealy",
+                    ifelse(POs$Buyer=="Kai Wells","Wells",
+                           ifelse(POs$Buyer=="Burma Jackson","Jackson",
+                                  ifelse(POs$Buyer=="Stephanie Warren","Warren",NA))))
 Buyers<-aggregate(WorkingDays~Qtr+Buyer2,data=POs,mean);Buyers$WorkingDays<-round(Buyers$WorkingDays,2)
+Buyers<-subset(Buyers,Qtr>"2012 Q4")
 Buyer_Plot<-ggplot(Buyers,aes(x=factor(Qtr),y=WorkingDays,group=Buyer2,color=factor(Buyer2)))+
     geom_line(stat="identity",size=1)+
       geom_hline(aes(yintercept=4,colour="#FF0000"),linetype=2,size=1)+
-        ylab("Business Days")+xlab("Quarter")+
-          geom_text(aes(label=ifelse(Qtr==r_period,WorkingDays,"")))
+        ggtitle("Business Days to Process POs per Buyer")+
+          ylab("Business Days")+xlab("Quarter")+
+            geom_text(aes(label=ifelse(Qtr==r_period,Buyer2,""),show_guide=FALSE))+
+                theme(legend.position="none",plot.title=element_text(size=13,face="bold",vjust=1))              
 print(Buyer_Plot)
-ggsave("./ReqtoCheckSTAT/Query Files/Slides/Procurement/Buyer Plot.png")
+ggsave("./ReqtoCheckSTAT/Query Files/Output/Slides/Procurement/Buyer Plot.png")
 
 ## Export the data
-write.csv(POs,"O:/Projects/ReqtoCheckSTAT/Query Files/Output/Purchase Orders.csv")
+write.csv(POs,"O:/Projects/ReqtoCheckSTAT/Query Files/Output/Procurement/POs.csv")
+write.csv(Buyers,"O:/Projects/ReqtoCheckSTAT/Query Files/Output/Procurement/Buyers.csv")
