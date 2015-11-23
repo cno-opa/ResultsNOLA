e22d2e2fa13b2892a8ced222ea417ff0e6ad1adc
unknown <vbspencer@nola.gov>
2015-11-23 12:22:27 -0600

    Cleaned up comments, removed position args for distributions

--- Bids_RFPs_DBEs.R
+++ Bids_RFPs_DBEs.R
@@ -6,13 +6,13 @@
 DBE<-read.csv("O:/Projects/ReqtoCheckStat/Query Files/DBE.csv")  
 Bids_RFPs<-read.csv("O:/Projects/ReqtoCheckStat/Query Files/Bids-RFPs.csv")
 
-### 
+### Data cleaning
 
-### Create quarter variable
+#### Create quarter variable
 Bids_RFPs$Qtr<-as.yearqtr(Bids_RFPs$EndMonth,"%m/%d/%Y")
 Bids_RFPs$First_Qtr<-as.yearqtr(Bids_RFPs$FirstMonth,"%m/%d/%Y")
 
-## Create plot of the bids opened, closed, and open at the end of each quarter
+#### Create plot of the bids opened, closed, and open at the end of each quarter
 Bid_Flow<-subset(Bids_RFPs,Type=="Bid")
 Bid_First_Summary<-data.frame(table(Bid_Flow$First_Qtr))
 Bid_End_Summary<-data.frame(table(Bid_Flow$Qtr))
@@ -33,7 +33,7 @@ Open_closedBidplot<-ggplot(summaryBid_melt,aes(x=factor(Qtr),y=value,fill=variab
 print(Open_closedBidplot)
 ggsave("./ReqtoCheckSTAT/Query Files/Slides/Bids-RFPs-DBEs/Opened_Closed_In Queue_Bids.png")
 
-## Create plot of the RFPs opened, closed, and open at the end of each quarter
+#### Create plot of the RFPs opened, closed, and open at the end of each quarter
 RFP_Flow<-subset(Bids_RFPs,Type=="RFP/Q")
 RFP_First_summary<-data.frame(table(RFP_Flow$First_Qtr))
 RFP_End_summary<-data.frame(table(RFP_Flow$Qtr))
@@ -54,7 +54,7 @@ Open_closedRFPplot<-ggplot(summaryRFP_melt,aes(x=factor(Qtr),y=value,fill=variab
 print(Open_closedRFPplot)
 ggsave("./ReqtoCheckSTAT/Query Files/Slides/Bids-RFPs-DBEs/Opened_Closed_In Queue_RFPs.png")
 
-### Create distribution bins of bids and proposals
+#### Create distribution bins of bids and proposals
 Bids_RFPs$BidUnder3<-ifelse(Bids_RFPs$Type=="Bid" & Bids_RFPs$Responses<=2,1,0)
     Bids_RFPs$Bid3_6<-ifelse(Bids_RFPs$Type=="Bid" & Bids_RFPs$Responses>2 & Bids_RFPs$Responses<=6,1,0)
     Bids_RFPs$Bid7_9<-ifelse(Bids_RFPs$Type=="Bid" & Bids_RFPs$Responses>6 & Bids_RFPs$Responses<=9,1,0)
@@ -64,7 +64,7 @@ Bids_RFPs$RFPUnder3<-ifelse(Bids_RFPs$Type=="RFP/Q" & Bids_RFPs$Responses<=2,1,0
     Bids_RFPs$RFP7_9<-ifelse(Bids_RFPs$Type=="RFP/Q" & Bids_RFPs$Responses>6 & Bids_RFPs$Responses<=9,1,0)
     Bids_RFPs$RFPOver10<-ifelse(Bids_RFPs$Type=="RFP/Q" & Bids_RFPs$Responses>=10,1,0)
 
-### Plot the average bid responses per quarter
+#### Plot the average bid responses per quarter
 Bidresponse<-subset(Bids_RFPs,Type=="Bid")
 Bidresponse<-aggregate(data=Bidresponse,Responses~Qtr,FUN=mean)
 Bidresponse_plot<-ggplot(Bidresponse,aes(x=factor(Qtr),y=Responses))
@@ -77,7 +77,7 @@ Bidresponse_plot<-Bidresponse_plot+geom_hline(aes(yintercept=3,colour="#FF0000")
 print(Bidresponse_plot)
 ggsave("./ReqtoCheckSTAT/Query Files/Slides/Bids-RFPs-DBEs/Responses per Bid.png")
 
-### Plot the average proposals per quarter
+#### Plot the average proposals per quarter
 RFPresponse<-subset(Bids_RFPs,Type=="RFP/Q")
 RFPresponse<-aggregate(data=RFPresponse,Responses~Qtr,FUN=mean)
 RFPresponse_plot<-ggplot(RFPresponse,aes(x=factor(Qtr),y=Responses))
@@ -90,55 +90,43 @@ RFPresponse_plot<-RFPresponse_plot+geom_hline(aes(yintercept=3,colour="#FF0000")
 print(RFPresponse_plot)
 ggsave("./ReqtoCheckSTAT/Query Files/Slides/Bids-RFPs-DBEs/Proposals per RFP.png")
 
-################
-## Subset and aggregate for bids, to prepare to plot distribution
+### Plotting - Distributions
+
+#####  Bid response distribution plot
 Bid_dist<-subset(Bids_RFPs,Type=="Bid")
 Bid_dist<-select(Bid_dist,Qtr,BidUnder3,Bid3_6,Bid7_9,BidOver10)
 Bid_dist<-aggregate(cbind(Bid_dist$BidUnder3, Bid_dist$Bid3_6, Bid_dist$Bid7_9, Bid_dist$BidOver10)~Qtr,data=Bid_dist,FUN=sum);colnames(Bid_dist)[grepl("V1", colnames(Bid_dist))] <- "Under3";colnames(Bid_dist)[grepl("V2", colnames(Bid_dist))] <- "Between3_6";colnames(Bid_dist)[grepl("V3", colnames(Bid_dist))] <- "Between7_9";colnames(Bid_dist)[grepl("V4", colnames(Bid_dist))] <- "Over10"
-
-### Calculate percentage of each bin
+#### Calculate percentage of each bin
 Bid_dist$Total<-Bid_dist$Under3+Bid_dist$Between3_6+Bid_dist$Between7_9+Bid_dist$Over10 ###Create column of total bids per quarter
     Bid_dist$Under3<-round(Bid_dist$Under3/Bid_dist$Total,3) ### Divide bin by total to get quarterly percentage 
     Bid_dist$Between3_6<-round(Bid_dist$Between3_6/Bid_dist$Total,3) ### Divide bin by total to get quarterly percentage 
     Bid_dist$Between7_9<-round(Bid_dist$Between7_9/Bid_dist$Total,3) ### Divide bin by total to get quarterly percentage 
     Bid_dist$Over10<-round(Bid_dist$Over10/Bid_dist$Total,3) ### Divide bin by total to get quarterly percentage 
-###
 Bid_dist<-select(Bid_dist,Qtr,Under3,Between3_6,Between7_9,Over10)
-##Define data label positions (heights) for distribution
-undermaxbid<-max(Bid_dist$Under3); 
-min4_6<-min(Bid_dist$Between4_6)+undermaxbid;
-min7_9<-min(Bid_dist$Between7_9)+min4_6;
-overmin_bid<-min(Bid_dist$Over10)+min7_9
 ### Melt data frame
 Bid_dist<-melt(Bid_dist,id.vars="Qtr")
 ### Calculate data label position height
-Bid_dist$position<-ifelse(Bid_dist$variable=="Under3",Bid_dist$value-.03,
-                           ifelse(Bid_dist$variable=="Between4_6",sum(undermaxbid, min4_6)/2,
-                                  ifelse(Bid_dist$variable=="Between7_9",sum(min4_6,min7_9)/2,.99))) 
-### Plot bids
+### Plot 
 Bid_dist_plot<-ggplot(Bid_dist,aes(x = factor(Qtr), y = value,fill = variable)) + 
   geom_bar(position = "stack",stat = "identity") + 
   scale_y_continuous(labels = percent_format())+
   ggtitle("Distribution of Bid Responses per Bid")+
   xlab("Quarters")+ylab("Percent")+
-  geom_text(aes(ymax=value,y=position,label=percent(value)),size=4)+
   scale_fill_manual(values=c(red,darkBlue,lightBlue,"green"),name=" ",labels=c("<=3 Bids","4-6 Bids","7-9 Bids","<=10 Bids"))
 print(Bid_dist_plot)
 ggsave("./ReqtoCheckSTAT/Query Files/Slides/Bids-RFPs-DBEs/Bid Distribution.png")
 
-#############
-## Subset and aggregate for bids, to prepare to plot distribution
+
+#### RFP response distribution plot
 RFP_dist<-subset(Bids_RFPs,Type=="RFP/Q")
 RFP_dist<-select(RFP_dist,Qtr,RFPUnder3,RFP3_6,RFP7_9,RFPOver10)
 RFP_dist<-aggregate(cbind(RFP_dist$RFPUnder3, RFP_dist$RFP3_6, RFP_dist$RFP7_9, RFP_dist$RFPOver10)~Qtr,data=RFP_dist,FUN=sum);colnames(RFP_dist)[grepl("V1", colnames(RFP_dist))] <- "Under3";colnames(RFP_dist)[grepl("V2", colnames(RFP_dist))] <- "Between3_6";colnames(RFP_dist)[grepl("V3", colnames(RFP_dist))] <- "Between7_9";colnames(RFP_dist)[grepl("V4", colnames(RFP_dist))] <- "Over10"
-
 ### Calculate percentage of each bin
 RFP_dist$Total<-RFP_dist$Under3+RFP_dist$Between3_6+RFP_dist$Between7_9+RFP_dist$Over10 ###Create column of total bids per quarter
-RFP_dist$Under3<-round(RFP_dist$Under3/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
-RFP_dist$Between3_6<-round(RFP_dist$Between3_6/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
-RFP_dist$Between7_9<-round(RFP_dist$Between7_9/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
-RFP_dist$Over10<-round(RFP_dist$Over10/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
-###
+  RFP_dist$Under3<-round(RFP_dist$Under3/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
+  RFP_dist$Between3_6<-round(RFP_dist$Between3_6/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
+  RFP_dist$Between7_9<-round(RFP_dist$Between7_9/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
+  RFP_dist$Over10<-round(RFP_dist$Over10/RFP_dist$Total,3) ### Divide bin by total to get quarterly percentage 
 RFP_dist<-select(RFP_dist,Qtr,Under3,Between3_6,Between7_9,Over10)
 ### Melt data frame
 RFP_dist<-melt(RFP_dist,id.vars="Qtr")
