62775e5fddc563b244ce4fd5ffba0b9628a6446b
unknown <vbspencer@nola.gov>
2016-05-11 13:12:42 -0500

    Created plots of days to execute

--- Contract_PO2.R
+++ Contract_PO2.R
@@ -19,61 +19,29 @@ con_notes<-narrative[str_detect(tolower(narrative$Note),"ordinance"),]
 po_comments<-po_comments[!duplicated(po_comments$PO),] # De-duplicate contracts
 con_notes<-con_notes[!duplicated(con_notes$PO),] # De-duplicate contracts
 con_notes<-con_notes[!(con_notes$PO %in% po_comments$PO),] # Drop any rows from notes that appear in comments dataframe
-ordinance<-rbind(po_comments,con_notes)
+ord_narrative<-rbind(po_comments,con_notes)
+
+##### Detect comments and notes in ECMS denoting contracts which have been manually routed via Expediency form
+manual_comments<-narrative[str_detect(tolower(narrative$Comments),"expediency")| str_detect(tolower(narrative$Comments),"manual"),]
+manual_comments<-manual_comments[!duplicated(manual_comments$PO),]
+manual_notes<-narrative[str_detect(tolower(narrative$Note),"expediency")| str_detect(tolower(narrative$Note),"manual"),]
+manual_notes<-manual_notes[!duplicated(manual_notes$PO),]
+manual<-rbind(manual_comments,manual_notes)
+
+##### Detect comments and notes indicating that a contract has been executed
+exec_comments<-narrative[narrative$Approver=="CONTRACTCLERK"|narrative$Approver=="CMESTRICKLAND"|narrative$Approver=="CMJAVERILL",]
+exec_comments<-exec_comments[!is.na(exec_comments$Comments),]
+exec_comments<-exec_comments[exec_comments$Comments!="",]
 
 
-#### Law
-##### Trim whitespace from AltID
-#Law$AltID<-str_trim(Law$AltID)
-
-
-### System data cleaning
-
-#### Contract requisitions 
-##### Subset contract reqs that were created during the analysis period
-# law_reqs<-req_routing[!duplicated(req_routing$Req),]
-# law_reqs<-law_reqs[as.Date(law_reqs$ReqComplete)>=first,]
-# law_reqs<-select(law_reqs,-Req)
-# 
-# ##### Code contract req status column
-# ##### De-duplicate reqs waiting for Law processing
-# req_status<-group_by(req_status_dates,Req)%>%
-#   mutate(current_status=max(ReqStatusDate))%>%
-#   filter(current_status==ReqStatusDate)%>%
-#   filter(MAJOR_STATUS_CODE=="1RRP"|MAJOR_STATUS_CODE=="1RGP")%>%
-#   select(Req,ReqStatusDate,MAJOR_STATUS_CODE)
-# 
-# ##### Create and code a contract req status column
-# req_status[,"ContractReqStatus"]<-NA
-# req_status$ContractReqStatus<-ifelse(req_status$MAJOR_STATUS_CODE=="1RRP","ReadyForLaw","ConvertedtoPO")
-# 
-# ##### Pivot into specific pre-law status columns
-# req_status<-select(req_status,-MAJOR_STATUS_CODE)%>%
-#   spread(ContractReqStatus,ReqStatusDate)
-# 
-# ##### Merge contract reqs 
-# law_reqs$Req<-as.factor(law_reqs$Req)
-# law_reqs<-left_join(law_reqs,req_status,by="Req")
-
-##### Create data frame of rows with no approval dates
-# pre_law_pos<-select(po_routing,PO,ApprovalDate) %>%
-#   filter(is.na(ApprovalDate))%>%
-#   select(PO)
-
+#### Approval data cleaning
 ##### Create data frame of dates attorneys completed legal review
-legal_review<-select(po_routing,PO,ReqComplete)
+legal_review<-select(po_routing,PO,LegalReview)
 legal_review<-legal_review[!duplicated(legal_review$PO),]
 
 ##### Clean contract routing dataset to prepare to pivot
-con_routing<-select(po_routing,-Comments,-ReqComplete) %>%
+con_routing<-select(po_routing,-Comments,-LegalReview) %>%
         filter(!is.na(ApprovalDate) & Ord<6)
-
-##### Filter pre_law list for contracts that have no approvals and/or path
-#pre_law_pos$PO<-ifelse(pre_law_pos$PO %in% po_routing$PO,NA,pre_law$PO)
-#pre_law_pos<-filter(pre_law_pos,!is.na(pre_law_pos$PO))
-#pre_law_pos<-data.frame(unique(pre_law_pos$PO))
-#pre_law_pos[,2:6]<-NA; names(pre_law_pos)<-c("PO","DepAttorney","CAO","SentVendor","ECMS_BackFromVendor","Publication")
-
 
 ##### Create stage column to code approval stages based on the order number
 con_routing[,"Stage"]<-NA 
@@ -99,7 +67,7 @@ for (i in 1:nrow(con_routing)){
     
   } else {
     
-    con_routing$Stage[i]<-"Publication"
+    con_routing$Stage[i]<-"Executive"
     
   }
 }
@@ -108,7 +76,7 @@ for (i in 1:nrow(con_routing)){
 con_routing<-select(con_routing,-Ord,-Approver)
 
 ##### Pivot approval column into separate columns for each stage
-con_routing<-select(spread(con_routing,Stage,ApprovalDate),PO,DepAttorney,CAO,SentVendor,ECMS_BackFromVendor,Publication)
+con_routing<-select(spread(con_routing,Stage,ApprovalDate),PO,DepAttorney,CAO,SentVendor,ECMS_BackFromVendor,Executive)
 
 ##### Elminate cancelled contracts from header dataframe
 po_header<-po_header[po_header$POStatus!="3PCA"& po_header$POStatus!="3PCR",]
@@ -123,8 +91,9 @@ contracts<-left_join(contracts,approval_paths,by="Dept_code")
 contracts<-left_join(contracts,adjust,by="PO")
 
 ### Eliminate contracts that have been cancelled, but are not reflected in system
-contracts<-filter(contracts,is.na(Cancelled))
 needs_cancel<-filter(contracts,!is.na(Cancelled))
+contracts<-filter(contracts,is.na(Cancelled))
+
 
 ### Eliminate contracts created after the end of the quarter
 contracts<-contracts[as.Date(contracts$ContractDate)<=last,]
@@ -132,7 +101,12 @@ contracts<-contracts[as.Date(contracts$ContractDate)<=last,]
 ### Code additional ordinance contracts identified through ECMS notes and comments
 contracts$Ordinance<-ifelse(!is.na(contracts$Ordinance),
                             contracts$Ordinance,
-                            ifelse(contracts$PO %in% ordinance$PO & is.na(contracts$Ordinance),1,NA))
+                            ifelse(contracts$PO %in% ord_narrative$PO & is.na(contracts$Ordinance),1,NA))
+
+### 
+contracts$Manual<-ifelse(startsWith(tolower(contracts$AltID),'m'),1,
+                         ifelse(contracts$PO %in% manual$PO,1,0))
+
 
 ##### Create consolidated start date column to account for contracts with an inaccurate contrat date in ECMS
 contracts[,"Start_Date"]<-NA
@@ -162,15 +136,15 @@ for (i in 1:nrow(contracts)){
     
     contracts$Close_Date[i]<-as.POSIXct(contracts$AdjustedSignDate[i])
     
-  } else if (!is.na(contracts$Publication[i])){
+  } else if (!is.na(contracts$Executive[i])){
     
-    contracts$Close_Date[i]<-as.POSIXct(contracts$Publication[i])
+    contracts$Close_Date[i]<-as.POSIXct(contracts$Executive[i])
     
-  } else if ( (is.na(contracts$Publication[i])) & (contracts$POStatus[i]=="3PS") ){
+  } else if ( (is.na(contracts$Executive[i])) & (contracts$POStatus[i]=="3PS") ){
     
     contracts$Close_Date[i]<-as.POSIXct(contracts$StatusDate[i])
     
-  } else if ( (is.na(contracts$Publication[i])) & (contracts$POStatus[i]=="3PRS") ){
+  } else if ( (is.na(contracts$Executive[i])) & (contracts$POStatus[i]=="3PRS") ){
     
     contracts$Close_Date[i]<-as.POSIXct(contracts$StatusDate[i])
     
@@ -186,39 +160,103 @@ for (i in 1:nrow(contracts)){
 contracts$Start_Qtr<-as.yearqtr(contracts$Start_Date)
 contracts$Close_Qtr<-as.yearqtr(contracts$Close_Date)
 
-##### Create dataframes of contracts that have been signed, as well as those that were open at the end of the quarter
-open_contracts<-contracts[is.na(contracts$Close_Date),]
-closed_contracts<-contracts[!is.na(contracts$Close_Date),]
-
 
 ## Closed contracts cleaning
 ##### Create days to execute column
-closed_contracts$Execute_Days<-as.integer(difftime(as.POSIXct(closed_contracts$Close_Date),as.POSIXct(closed_contracts$Start_Date),units="days"))
+contracts$Execute_Days<-round(as.numeric(difftime(as.POSIXct(contracts$Close_Date),as.POSIXct(contracts$Start_Date),units="days")),1)
 
 ##### Calculate days per approval stage 
-closed_contracts$Legal_Days<-as.integer(difftime(as.POSIXct(closed_contracts$ReqComplete),as.POSIXct(closed_contracts$ContractDate),units="days"))
+contracts$Legal_Days<-round(as.numeric(difftime(as.POSIXct(contracts$LegalReview),as.POSIXct(contracts$ContractDate),units="days")),1)
 
-closed_contracts$Dep_Attorney_Days<-as.integer(difftime(as.POSIXct(closed_contracts$DepAttorney),as.POSIXct(closed_contracts$ReqComplete),units="days"))
+contracts$Dep_Attorney_Days<-round(as.numeric(difftime(as.POSIXct(contracts$DepAttorney),as.POSIXct(contracts$LegalReview),units="days")),1)
 
-closed_contracts$CAO_Days<-as.integer(difftime(as.POSIXct(closed_contracts$CAO),as.POSIXct(closed_contracts$DepAttorney),units="days"))
+contracts$CAO_Days<-round(as.numeric(difftime(as.POSIXct(contracts$CAO),as.POSIXct(contracts$DepAttorney),units="days")),1)
 
-closed_contracts$Sent_Vendor_Days<-ifelse(is.na(closed_contracts$Ordinance)
-                    ,as.integer(difftime(as.POSIXct(closed_contracts$SentVendor),as.POSIXct(closed_contracts$CAO),units="days"))
-                    ,NA)
+contracts$Sent_Vendor_Days<-round(as.numeric(difftime(as.POSIXct(contracts$SentVendor),as.POSIXct(contracts$CAO),units="days")),1)
 
-closed_contracts$Awaiting_Vendor_Days<-ifelse(!is.na(closed_contracts$BackFromVendor)
-                    ,as.integer(difftime(as.POSIXct(closed_contracts$BackFromVendor),as.POSIXct(closed_contracts$SentVendor),units="days"))
-                    ,as.integer(difftime(as.POSIXct(closed_contracts$ECMS_BackFromVendor),as.POSIXct(closed_contracts$SentVendor),units="days")))
+contracts$Awaiting_Vendor_Days<-round(as.numeric(difftime(as.POSIXct(contracts$ECMS_BackFromVendor),as.POSIXct(contracts$SentVendor),units="days")))
 
-closed_contracts$Executive_Signature_Days<-as.integer(difftime(as.POSIXct(closed_contracts$Close_Date),as.POSIXct(closed_contracts$ECMS_BackFromVendor),units="days"))
+contracts$Executive_Signature_Days<-round(as.numeric(difftime(as.POSIXct(contracts$Close_Date),as.POSIXct(contracts$ECMS_BackFromVendor),units="days"))
 
-closed_contracts$Publication_Days<-ifelse(!is.na(closed_contracts$AdjustedSignDate) & !is.na(closed_contracts$Publication)
-                                          ,as.integer(difftime(as.POSIXct(closed_contracts$Publication),as.POSIXct(closed_contracts$Close_Date),units="days"))
+contracts$Executive_Days<-ifelse(!is.na(contracts$AdjustedSignDate) & !is.na(contracts$Executive)
+                                          ,round(as.numeric(difftime(as.POSIXct(contracts$Executive),as.POSIXct(contracts$Close_Date),units="days")),1)
                                           ,NA)
 
 
-### Export data to spreadsheets
-write.csv(contracts,"O:/Projects/ReqtoCheckStat/Query Files/Output/contracts.csv")
-write.xlsx(select(closed_contracts,PO,AltID,Dept,Vendor,Description,),
-           "O:/Projects/"
+#### Parse out contracts requiring a City Council Ordinance as a separate dataset 
+ordinance<-contracts[contracts$Ordinance==1,]
+contracts<-anti_join(contracts,ordinance,by="PO")
 
+### Plotting
+
+#### Bar charts of days to excute
+##### Non-Ordinance days to execute
+Days2Execute<-ddply(subset(contracts,!is.na(Execute_Days)),"Close_Qtr",summarise,Sign=mean(Execute_Days))
+Days2Execute<-subset(Days2Execute,Close_Qtr>"2012 Q2")
+Execution<-ggplot(Days2Execute,aes(x=factor(Close_Qtr),y=Sign))+
+  geom_bar(stat="identity",fill="steelblue")+
+  ggtitle("Average Days to Execute Contracts by Quarter \n - Non-Ordinance")+
+  xlab("Quarters")+ylab("Days")+
+  geom_text(aes(y=Sign,ymax=Sign+1,label=round(Sign,1)),position=position_dodge(width=0.9),vjust=-.5,size=5)+
+  geom_hline(aes(yintercept=30),colour="#FF0000",linetype=2,size=1.2)+theme(legend.position="Top")+
+  theme(plot.title=element_text(size=13,face="bold",vjust=1))
+print(Execution)
+ggsave("O:/Projects/ReqtoCheckStat/Query Files/Slides/Contract POs/Days to Execute - Non-Ordinance.png")
+
+##### Ordinance days to execute
+Ord_Days2Execute<-ddply(subset(ordinance,!is.na(Execute_Days)),"Close_Qtr",summarise,Sign=mean(Execute_Days))
+Ord_Days2Execute<-subset(Ord_Days2Execute,Close_Qtr>"2012 Q2")
+Ord_Execution<-ggplot(Ord_Days2Execute,aes(x=factor(Close_Qtr),y=Sign))+
+  geom_bar(stat="identity",fill="steelblue")+
+  ggtitle("Average Days to Execute Contracts by Quarter \n - Ordinance")+
+  xlab("Quarters")+ylab("Days")+
+  geom_text(aes(y=Sign,ymax=Sign+1,label=round(Sign,1)),position=position_dodge(width=0.9),vjust=-.5,size=5)+
+  theme(plot.title=element_text(size=13,face="bold",vjust=1))
+print(Ord_Execution)
+ggsave("O:/Projects/ReqtoCheckStat/Query Files/Slides/Contract POs/Days to Execute - Ordinance.png")
+
+
+#### Stacked bars of days to execute
+##### Create bins of day distributions
+contracts$Under30<-ifelse(contracts$Execute_Days<=30,1,0)
+contracts$Between31_60<-ifelse(contracts$Execute_Days>30 & contracts$Execute_Days<=60,1,0)
+contracts$Between61_90<-ifelse(contracts$Execute_Days>60 & contracts$Execute_Days<=90,1,0)
+contracts$Between91_120<-ifelse(contracts$Execute_Days>90 & contracts$Execute_Days<=120,1,0)
+contracts$Over120<-ifelse(contracts$Execute_Days>120,1,0)
+
+##### Aggregation and melting
+con_PO_dist<-select(contracts,Close_Qtr,Under30,Between31_60,Between61_90,Between91_120,Over120)
+con_PO_dist<-aggregate(cbind(con_PO_dist$Under30,con_PO_dist$Between31_60,con_PO_dist$Between61_90,con_PO_dist$Between91_120,con_PO_dist$Over120)~Close_Qtr,data=con_PO_dist,FUN=sum);colnames(con_PO_dist)[grepl("V1", colnames(con_PO_dist))] <- "<=30";colnames(con_PO_dist)[grepl("V2", colnames(con_PO_dist))] <- "31-60";colnames(con_PO_dist)[grepl("V3", colnames(con_PO_dist))] <- "61-90";colnames(con_PO_dist)[grepl("V4", colnames(con_PO_dist))] <- "91-120";colnames(con_PO_dist)[grepl("V5", colnames(con_PO_dist))] <- ">120"
+con_PO_dist<-subset(con_PO_dist,Close_Qtr>"2012 Q2")
+con_PO_dist<-melt(con_PO_dist,id.vars="Close_Qtr",variable.name="Days")
+
+con_PO_dist<-con_PO_dist %>% group_by(Close_Qtr, Days) %>% 
+  summarise(value = sum(value)) %>%   # Within each Brand, sum all values in each Category
+  mutate(percent = value/sum(value),
+         pos = cumsum(percent) - 0.5*percent)
+
+Execute_dist_plot<-ggplot(con_PO_dist, aes(x=factor(Close_Qtr),y=percent, fill=Days)) +
+  geom_bar(stat='identity',  width = .7, colour="black", lwd=0.1) +
+  geom_text(aes(label=ifelse(percent >= 0.01, paste0(sprintf("%.0f", percent*100),"%"),""),
+                y=pos), colour="black",size=4) +
+  scale_y_continuous(labels = percent_format()) +
+  scale_fill_manual(values=c("#009900","#FFFFCC","#FFCC99","#FF9999","#FF3300"))+
+  labs(y="Distribution", x="Quarter")+
+  ggtitle("Distribution of Days to Execute (Non-Ordinance)")+
+  theme(plot.title=element_text(size=13,face="bold",vjust=1))
+print(Execute_dist_plot)
+ggsave("O:/Projects/ReqtoCheckStat/Query Files/Slides/Contract POs/Execute Distribution.png")
+
+
+#### Create subsets of data for sending to Law and Executive Counsel
+##### Create dataframes of contracts that have been signed, as well as those that were open at the end of the quarter
+open_contracts<-contracts[is.na(contracts$Close_Qtr)|contracts$Close_Qtr>as.yearqtr(last),]
+closed_contracts<-contracts[contracts$Close_Qtr<=as.yearqtr(last),]
+
+##### 
+Exec_queue<-contracts[is.na(contracts$Executive) & contracts$POStatus=="3PRA" & !is.na(contracts$ECMS_BackFromVendor),]
+
+### Export data to spreadsheets
+write.csv(Exec_queue,"O:/Projects/ReqtoCheckStat/Query Files/Output/Contract POs/Exec counsel queue.csv")
+write.csv(contracts,"O:/Projects/ReqtoCheckStat/Query Files/Output/Contract POs/contracts.csv",na="")
+write.csv(narrative,"O:/Projects/ReqtoCheckStat/Query Files/Output/Contract POs/narrative.csv")
